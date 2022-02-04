a6073d2fae168725e21b34634a12187459937a86
Gabriel Cavalheiro <gccavalheiro94@gmail.com>
2022-02-04 14:16:10 -0300

    chore: integrando as funcionalidades

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,8 +7,11 @@ import { Container, Cart } from './styles';
 import { useCart } from '../../hooks/useCart';
 
 const Header = (): JSX.Element => {
-  // const { cart } = useCart();
-  // const cartSize = // TODO;
+  const { cart } = useCart();
+  const cartSize = cart.length
+  console.log('----cartSize----')
+  console.log({ cart }, cartSize);
+  console.log(cartSize);
 
   return (
     <Container>
@@ -20,7 +23,7 @@ const Header = (): JSX.Element => {
         <div>
           <strong>Meu carrinho</strong>
           <span data-testid="cart-size">
-            {/* {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} */}
+            {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
           </span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
